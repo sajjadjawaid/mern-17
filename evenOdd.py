1ecb6f527ff361f645fb3eb12e744f38da578783
sajjadjawaid <sajjadjawaid@gmail.com>
2024-05-02 14:32:32 +0500

    commit for branch 5

--- evenOdd.py
+++ evenOdd.py
@@ -5,3 +5,21 @@ if(a % 2 == 0):
 
 else:
     print("it is not even.")
+
+
+
+a = 4
+
+if(a % 2 == 0):
+    print("it is even.")
+
+else:
+    print("it is not even.")
+
+a = 4
+
+if(a % 2 == 0):
+    print("it is even.")
+
+else:
+    print("it is not even.")
